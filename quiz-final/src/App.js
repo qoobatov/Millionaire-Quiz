3f3f09149f47c69d4cf2ff54aa9e7eccde601b4b
Erkin Kubatov <qoobatov@gmail.com>
2022-09-15 20:11:37 +0600

    mapped moneys and added component

--- quiz-final/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      Quiz app
-    </div>
-  );
-}
-
-export default App;
---
+++ quiz-final/src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export default function Trivia() {
+  return (
+    <div className="trivia">
+        <div className="question">Столица Кыргызстна?</div>
+        <div className="answers">
+            <div className="answer">Бишкек</div>
+            <div className="answer">Бишкек</div>
+            <div className="answer">Бишкек</div>
+            <div className="answer">Бишкек</div>
+        </div>
+    </div>
+  )
+}
